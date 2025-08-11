ce652825c3758771c07fa1b26d11ab87df1a869f
Pascal Bauermeister <pascal.bauermeister@gmail.com>
2025-08-11 22:32:24 +0200

    Arduino sketch works for 1 servo on pin 9.

--- 3d-printing/2024-09-29--servo-clocks/test-geekservo-sweep/test-geekservo-sweep.ino
+++ 3d-printing/2024-09-29--servo-clocks/test-geekservo-sweep/test-geekservo-sweep.ino
@@ -1,16 +1,4 @@
-/* Sweep
- by BARRAGAN <http://barraganstudio.com>
- This example code is in the public domain.
-
- modified 8 Nov 2013
- by Scott Fitzgerald
- http://www.arduino.cc/en/Tutorial/Sweep
-*/
-
 #include <Servo.h>
-
-Servo myservo;  // create servo object to control a servo
-// twelve servo objects can be created on most boards
 
 const int P1 = 0;    // 35;
 const int P2 = 58;   // 95;
@@ -22,69 +10,98 @@ const int offset = 0;
 #define DELAY 20
 #define DELAY2 1000
 
-void turn(int from, int to) { turn(from, to, DELAY2); }
+class GeekServo {
+public:
+  GeekServo(int pin);
+  void turn(int from, int to);
+  void turn(int from, int to, int final_delay);
 
-void turn(int from, int to, int final_delay) {
+private:
+  Servo servo;
+};
+
+GeekServo::GeekServo(int pin) {
+  servo.attach(pin);
+
+  const int final_delay = 100;
+  const int p1 = P3;  // neutral position
+  const int p2 = p1 + 10;
+
+  // shake
+  turn(p1, p2, final_delay);
+  turn(p2, p1, final_delay);
+  turn(p1, p2, final_delay);
+  turn(p2, p1, final_delay);
+}
+
+void GeekServo::turn(int from, int to) {
+  turn(from, to, DELAY2);
+}
+
+void GeekServo::turn(int from, int to, int final_delay) {
   from -= P1;
   to -= P1;
   from += offset;
   to += offset;
 
   if (from == to) {
-  }
-  if (from < to) {
+  } else if (from < to) {
     for (int pos = from; pos <= to; pos++) {
-      myservo.write(pos);
+      Serial.println("GeekServo::turn inc");
+      servo.write(pos);
       delay(DELAY);
     }
   } else {
     for (int pos = from; pos >= to; pos--) {
-      myservo.write(pos);
+      Serial.println("GeekServo::turn dec");
+      servo.write(pos);
       delay(DELAY);
     }
   }
 
   // final shake
   for (int amp = SHAKE; amp; amp = amp / 2) {
-    myservo.write(to + amp);
+    servo.write(to + amp);
     delay(DELAY / 2 * amp);
-    myservo.write(to - amp);
+    servo.write(to - amp);
     delay(DELAY / 2 * amp);
   }
-  myservo.write(to);
+  servo.write(to);
 
   delay(final_delay);  // wait for a while before next move
 }
 
+GeekServo *gs1;
+
+void toggle_led() {
+  static int state = -1;
+  if (state == -1)
+    pinMode(LED_BUILTIN, OUTPUT);
+  state = !state;
+  digitalWrite(LED_BUILTIN, state ? HIGH : LOW);
+}
+
 void setup() {
-  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
-  myservo.write(0);
-  // delay(1000);
-  const int final_delay = 100;
-  const int p1 = P3;
-  const int p2 = p1+10;
-  turn(p1, p2, final_delay);
-  turn(p2, p1, final_delay);
-  turn(p1, p2, final_delay);
-  turn(p2, p1, final_delay);
+  for (int i = 0; i < 20; ++i) {
+    toggle_led();
+    delay(100);
+  }
+  gs1 = new GeekServo(9);
   delay(2000);  // give opportunity to start upload w/o servo consumption
 }
 
 void loop() {
-  turn(P1, P2);
-  turn(P2, P3);
-  turn(P3, P4);
+  toggle_led();
+  gs1->turn(P1, P2);
+  toggle_led();
+  gs1->turn(P2, P3);
+  toggle_led();
+  gs1->turn(P3, P4);
 
-  turn(P4, P3);
-  turn(P3, P2);
-  turn(P2, P1);
-  return;
-  turn(P1, P1);
-  turn(P1, P2);
-  turn(P2, P3);
-  //  turn(P3, P4);
-
-  //  turn(P4, P3);
-  turn(P3, P2);
-  turn(P2, P1);
+  toggle_led();
+  gs1->turn(P4, P3);
+  toggle_led();
+  gs1->turn(P3, P2);
+  toggle_led();
+  gs1->turn(P2, P1);
 }
